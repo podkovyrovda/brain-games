770894093deab825ae988b2b21f394f83416102d
podkovyrovda <a.podkovyrov@yandex.ru>
2019-02-05 20:56:50 +0300

    Fixed mistakes index.js brain-games.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => { 
-    const userName = readlineSync.question('May I have your name? ');
-    const greeting = console.log('Hello, ' + userName + '!');
-}
-
-export { greeting }
+export default () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
