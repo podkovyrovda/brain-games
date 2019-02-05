43d607c3ddbe35365b1df0f00e62d7cca186791a
podkovyrovda <a.podkovyrov@yandex.ru>
2019-02-05 22:06:23 +0300

    First try for brain-even.js

---
+++ src/bin/brain-even.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import greeting from '..';
+import padding from '..';
+import isEven from '..';
+
+console.log('Welcome to the Brain Games! ');
+
+padding();
+
+console.log('Answer \"yes\" if number even otherwise answer \"no\"')
+
+greeting();
+
+isEven();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+const greeting = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
+
+const padding = () => console.log('\n');
+
+const isEven = () => {
+  let num = Math.random();
+
+  const correctAnswer = console.log('Correct!');
+  const wrongAnswer = console.log(`\'${userAnswer}\' is wrong answer ;(. Correct answer was \'${showCorrectAnswer}\'.`)
+
+  const showCorrectAnswer = () => {
+    if (num % 2 === 0) { 
+      return  "yes"
+    };
+    return "no"
+  };
+
+  const userAnswer = readlineSync.question('Question: ${num} /n Your answer: ');
+  if (isNaN(userAnswer)) {
+
+  }
+};
+
+export { greeting, padding, isEven }
