57145640262916893d5b21966b097a9a4f79f34c
podkovyrovda <a.podkovyrov@yandex.ru>
2019-02-08 19:02:00 +0300

    added brain-progression

---
+++ src/games/brain-progression.js
@@ -0,0 +1,41 @@
+import { cons, car, cdr } from 'hexlet-pairs';
+import { getRandomNumInRange } from '../utils';
+import makeGame from '../engine';
+
+const greeting = 'What number is missing in the progression\n';
+
+const condition = () => {
+  const startOfRange = 1;
+  const endOfRange = 10;
+  const startOfProg = getRandomNumInRange(startOfRange, endOfRange);
+  const stepOfProg = getRandomNumInRange(startOfRange, endOfRange);
+
+  const elementsInProg = 10;
+  const randomElem = getRandomNumInRange(1, elementsInProg);
+
+  const getProgression = () => {
+    let numInProg = startOfProg;
+    let strOfProg = '';
+    let missingNum = 0;
+
+    for (let i = 1; i <= elementsInProg; i += 1) {
+      if (i === randomElem) {
+        missingNum = numInProg;
+        strOfProg += '.. ';
+        numInProg += stepOfProg;
+      } else {
+        strOfProg += `${numInProg} `;
+        numInProg += stepOfProg;
+      }
+    }
+    return cons(strOfProg, missingNum);
+  };
+
+  const progression = getProgression(startOfProg, stepOfProg);
+  const correctAnswer = cdr(progression);
+  const question = car(progression);
+
+  return cons(question, correctAnswer);
+};
+
+export default () => makeGame(greeting, condition);
